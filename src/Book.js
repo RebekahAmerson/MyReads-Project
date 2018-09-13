0ee211ba7ebe9edb5491243425e54afdf7fa7743
Rebekah Amerson <becka_811@hotmail.com>
2018-09-13 16:33:50 -0500

    BUGFIX: no image or author info

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import noCover from './icons/no-cover.png';
 
 class Book extends Component {
   changeShelf = (shelf, book) => {
@@ -7,12 +8,25 @@ class Book extends Component {
     }
   }
 
+checkImage(book) {
+  if (!book.hasOwnProperty('imageLinks')) {
+    book.imageLinks = {thumbnail: noCover};
+  }
+}
+
+checkAuthor(book) {
+  if (!book.hasOwnProperty('authors')) {
+    book.authors = 'Unknown';
+  }
+}
+
   render() {
     const {books} = this.props;
 
     return (
       books.map((book) => (
         <li key={book.id}>
+        {this.checkImage(book)}
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url("+ book.imageLinks.thumbnail +")" }}></div>
@@ -26,6 +40,7 @@ class Book extends Component {
               </select>
             </div>
           </div>
+          {this.checkAuthor(book)}
           <div className="book-title">{ book.title }</div>
           <div className="book-authors">{ book.authors }</div>
         </div>
